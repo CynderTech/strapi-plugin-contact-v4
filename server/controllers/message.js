aa2b7ef8bdcef718dd98d105b61222aa4ec32840
Bea M <64287361+beamariano@users.noreply.github.com>
2022-09-06 16:15:07 +0800

    fix: CRUD controllers and services

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -1,4 +1,5 @@
-'use strict';
+/* eslint-disable import/no-extraneous-dependencies */
+
 /**
  *   controller
  */
@@ -6,111 +7,98 @@
 /**
  * Read the Strapi v4 documentation (https://docs.strapi.io/developer-docs/latest/development/backend-customization/controllers.html) to customize this controller
  */
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('plugin::strapi-plugin-contact-v4.message', {
-	async count(ctx) {
-		ctx.body = strapi.plugin('plugin::strapi-plugin-contact-v4').service('message').count();
-	},
-
-	async find(ctx) {
-		ctx.body = 'FIND';
-		// let entities;
-		// if (ctx.query._q) {
-		// 	entities = await strapi.plugins[pluginId].services.message.search(ctx.query);
-		// } else {
-		// 	entities = await strapi.plugins[pluginId].services.message.find(ctx.query);
-		// }
-		// return entities.map((entity) =>
-		// 	sanitizeEntity(entity, {
-		// 		model: strapi.plugins[pluginId].models.message,
-		// 	}),
-		// );
-	},
-
-	/**
-	 * Retrieve a record.
-	 *
-	 * @return {Object}
-	 */
-	async findOne(ctx) {
-		ctx.body = 'FIND ONE';
-
-		// const { id } = ctx.params;
-		// const entity = await strapi.plugins[pluginId].services.message.findOne({
-		// 	id,
-		// });
-		// return sanitizeEntity(entity, {
-		// 	model: strapi.plugins[pluginId].models.message,
-		// });
-	},
-
-	/**
-	 * Create a record.
-	 *
-	 * @return {Object}
-	 */
-
-	async create(ctx) {
-		ctx.body = 'CREATE';
-
-		// let entity;
-		// if (ctx.is('multipart')) {
-		// 	const { data, files } = parseMultipartData(ctx);
-		// 	entity = await strapi.plugins[pluginId].services.message.create(data, {
-		// 		files,
-		// 	});
-		// } else {
-		// 	entity = await strapi.plugins[pluginId].services.message.create(ctx.request.body);
-		// }
-		// return sanitizeEntity(entity, {
-		// 	model: strapi.plugins[pluginId].models.message,
-		// });
-	},
-
-	/**
-	 * Update a record.
-	 *
-	 * @return {Object}
-	 */
-
-	async update(ctx) {
-		ctx.body = 'UPDATE';
-
-		// const { id } = ctx.params;
-		// let entity;
-		// if (ctx.is('multipart')) {
-		// 	const { data, files } = parseMultipartData(ctx);
-		// 	entity = await strapi.plugins[pluginId].services.message.update({ id }, data, {
-		// 		files,
-		// 	});
-		// } else {
-		// 	entity = await strapi.plugins[pluginId].services.message.update(
-		// 		{ id },
-		// 		ctx.request.body,
-		// 	);
-		// }
-		// return sanitizeEntity(entity, {
-		// 	model: strapi.plugins[pluginId].models.message,
-		// });
-	},
-
-	/**
-	 * Delete a record.
-	 *
-	 * @return {Object}
-	 */
-
-	async delete(ctx) {
-		ctx.body = 'DELETE';
-
-		// 	const { id } = ctx.params;
-		// 	const entity = await strapi.plugins[pluginId].services.message.delete({
-		// 		id,
-		// 	});
-		// 	return sanitizeEntity(entity, {
-		// 		model: strapi.plugins[pluginId].models.message,
-		// 	});
-		// },
-	},
-});
+ const { createCoreController } = require('@strapi/strapi').factories;
+ const pluginId = require('../../admin/src/pluginId');
+ 
+ module.exports = createCoreController('plugin::strapi-plugin-contact-v4.message', ({ strapi }) => ({
+	 /**
+		* Count records.
+		*
+		* @return {Number}
+		*/
+	 async count(ctx) {
+		 // if (ctx.query._q) {
+		 // 	return strapi.plugin(pluginId).service('message').countSearch(ctx.query);
+		 // }
+		 return strapi.plugin(pluginId).service('message').count(ctx);
+	 },
+ 
+	 /**
+		* Retrieve records.
+		*
+		* @return {Array}
+		*/
+	 async find(ctx) {
+		 return strapi.plugin(pluginId).service('message').find(ctx.query);
+	 },
+ 
+	 /**
+		* Retrieve a record.
+		*
+		* @return {Object}
+		*/
+	 async findOne(ctx) {
+		 const { id } = ctx.params;
+		 ctx.body = await strapi.plugin(pluginId).service('message').findOne(id);
+	 },
+ 
+	 /**
+		* Create a record.
+		*
+		* @return {Object}
+		*/
+	 async create(ctx) {
+		 const data = ctx.request.body;
+		 ctx.body = await strapi.plugin(pluginId).service('message').create(data);
+		 // let entity;
+		 // if (ctx.is('multipart')) {
+		 // 	const { data, files } = parseMultipartData(ctx);
+		 // 	entity = await strapi.plugins[pluginId].services.message.create(data, {
+		 // 		files,
+		 // 	});
+		 // } else {
+		 // 	entity = await strapi.plugins[pluginId].services.message.create(ctx.request.body);
+		 // }
+		 // return sanitizeEntity(entity, {
+		 // 	model: strapi.plugins[pluginId].models.message,
+		 // });
+	 },
+ 
+	 /**
+		* Update a record.
+		*
+		* @return {Object}
+		*/
+	 async update(ctx) {
+		 const { id } = ctx.params;
+		 const data = ctx.request.body;
+		 ctx.body = await strapi.plugin(pluginId).service('message').update(id, data);
+		 // const { id } = ctx.params;
+		 // let entity;
+		 // if (ctx.is('multipart')) {
+		 // 	const { data, files } = parseMultipartData(ctx);
+		 // 	entity = await strapi.plugins[pluginId].services.message.update({ id }, data, {
+		 // 		files,
+		 // 	});
+		 // } else {
+		 // 	entity = await strapi.plugins[pluginId].services.message.update(
+		 // 		{ id },
+		 // 		ctx.request.body,
+		 // 	);
+		 // }
+		 // return sanitizeEntity(entity, {
+		 // 	model: strapi.plugins[pluginId].models.message,
+		 // });
+	 },
+ 
+	 /**
+		* Delete a record.
+		*
+		* @return {Object}
+		*/
+	 async delete(ctx) {
+		 const { id } = ctx.params;
+		 ctx.body = await strapi.plugin(pluginId).service('message').delete(id);
+	 },
+ }));
+ 
--- server/services/message.js
+++ server/services/message.js
@@ -1,8 +1,83 @@
-module.exports = ({ strapi }) => ({
-	getWelcomeMessage() {
-		return 'Welcome to Strapi 🚀';
-	},
-	async count() {
-		return await strapi.query('plugin::strapi-plugin-contact-v4.message').count();
-	},
-});
+/* eslint-disable import/no-extraneous-dependencies */
+
+/**
+ *   service
+ */
+
+/**
+ * Read the documentation (https://docs.strapi.io/developer-docs/latest/development/backend-customization/services.html)
+ * to customize this service
+ */
+
+ const { createCoreController } = require('@strapi/strapi').factories;
+
+ module.exports = createCoreController('plugin::strapi-plugin-contact-v4.message', ({ strapi }) => ({
+	 /**
+		* Promise to count records
+		*
+		* @return {Promise}
+		*/
+	 async count(params) {
+		 return strapi.query('plugin::strapi-plugin-contact-v4.message').count(params);
+	 },
+ 
+	 /**
+		* Promise to count searched records
+		*
+		* @return {Promise}
+		*/
+	 async countSearch(params) {
+		 return strapi.query('plugin::strapi-plugin-contact-v4.message').countSearch(params);
+	 },
+ 
+	 /**
+		* Promise to fetch all records
+		*
+		* @return {Promise}
+		*/
+	 async find(query) {
+		 return strapi.entityService.findMany('plugin::strapi-plugin-contact-v4.message', query);
+	 },
+ 
+	 /**
+		* Promise to fetch one record
+		*
+		* @return {Promise}
+		*/
+	 async findOne(params) {
+		 console.log('params passed', params);
+		 return strapi.entityService.findOne('plugin::strapi-plugin-contact-v4.message', params);
+	 },
+ 
+	 /**
+		* Promise to add record
+		*
+		* @return {Promise}
+		*/
+	 async create(data) {
+		 return strapi.entityService.create('plugin::strapi-plugin-contact-v4.message', {
+			 data,
+		 });
+	 },
+ 
+	 /**
+		* Promise to edit record
+		*
+		* @return {Promise}
+		*/
+	 async update(id, data) {
+		 return strapi.entityService.update('plugin::strapi-plugin-contact-v4.message', id, {
+			 data,
+		 });
+	 },
+ 
+	 // /**
+	 //  * Promise to delete a record
+	 //  *
+	 //  * @return {Promise}
+	 //  */
+	 async delete(id) {
+		 return strapi.entityService.delete('plugin::strapi-plugin-contact-v4.message', id);
+	 },
+ }));
+ 
